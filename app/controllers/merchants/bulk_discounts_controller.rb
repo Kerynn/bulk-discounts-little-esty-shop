b218562881efbeb518d4a33780cd8091d4e53e40
Kerynn Davis <kerynn.davis@gmail.com>
2023-01-12 15:58:33 -0800

    add new discount form and ability to submit to merchant bulk disocunts index

--- app/controllers/merchants/bulk_discounts_controller.rb
+++ app/controllers/merchants/bulk_discounts_controller.rb
@@ -14,10 +14,19 @@ class Merchants::BulkDiscountsController < ApplicationController
     merchant
   end
 
+  def create 
+    merchant 
+    bulk_discount = merchant.bulk_discounts.create(bk_params)
+    redirect_to merchant_bulk_discounts_path(merchant)
+  end
 
   private
 
   def merchant
     @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def bk_params 
+    params.permit(:discount, :quantity_threshold)
   end
 end
