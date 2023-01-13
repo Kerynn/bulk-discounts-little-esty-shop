98bbfe42d0b7637f062b3f64e47dd18939ec1348
Kerynn Davis <kerynn.davis@gmail.com>
2023-01-13 15:26:14 -0800

    add holiday service class

---
+++ app/poro/holiday_search.rb
@@ -0,0 +1,15 @@
+require './app/poro/holiday'
+require './app/services/holiday_service'
+
+class HolidaySearch 
+
+  def holidays 
+    service.next_holidays[1..3].map do |data|
+      Holiday.new(data)
+    end
+  end
+
+  def service 
+    HolidayService.new
+  end
+end
