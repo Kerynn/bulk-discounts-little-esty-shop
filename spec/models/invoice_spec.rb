0aefc8e19b12d4b31f3d096828159e845a8dc26b
Kerynn Davis <kerynn.davis@gmail.com>
2023-01-13 17:35:57 -0800

    add testing for invoice discount in invoice model spec

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -9,18 +9,6 @@ RSpec.describe Invoice, type: :model do
     it {should have_many(:merchants).through(:items)}
   end
 
-  describe 'instance methods' do
-    describe '#total_revenue' do
-      it 'returns the total revenue for an invoice' do
-        invoice_1 = create(:invoice_with_items, item_count: 2, ii_qty: 5, ii_price: 3000)
-        invoice_2 = create(:invoice_with_items, item_count: 3, ii_qty: 2, ii_price: 2500)
-
-        expect(invoice_1.total_revenue).to eq(30000)
-        expect(invoice_2.total_revenue).to eq(15000)
-      end
-    end
-  end
-
   describe 'class methods' do 
     describe 'incomplete_invoices' do 
       it 'returns the invoices with items not yet shipped in order from oldest to youngest' do 
@@ -35,4 +23,48 @@ RSpec.describe Invoice, type: :model do
       end
     end
   end
+
+  describe 'instance methods' do
+
+    let!(:merchant_4) { Merchant.create!(name: "Jim") }
+    let!(:item_2) { merchant_4.items.create!(name: "Pens", description: "For Writing", unit_price: 200) }
+    let!(:item_3) { merchant_4.items.create!(name: "Paper", description: "The other thing you need for writing", unit_price: 400) }
+
+    let!(:bk_1) { merchant_4.bulk_discounts.create!(discount: 20, quantity_threshold: 15) }
+    let!(:bk_2) { merchant_4.bulk_discounts.create!(discount: 30, quantity_threshold: 20) }
+
+    let!(:merchant_5) { Merchant.create!(name: "Charlie") }
+    let!(:item_4) { merchant_5.items.create!(name: "Candy", description: "Chocolatey", unit_price: 500) }
+
+    let!(:bk_3) { merchant_5.bulk_discounts.create!(discount: 10, quantity_threshold: 5) }
+
+    let!(:cust_1) { Customer.create!(first_name: "Kamee", last_name: "Moore") }
+    let!(:cust_2) { Customer.create!(first_name: "Ozzie", last_name: "Jones") }
+
+    let!(:invoice_3) { cust_1.invoices.create!(status: 1) }
+    let!(:ii_1) { InvoiceItem.create!(quantity: 10, unit_price: 200, status: 2, item_id: item_2.id, invoice_id: invoice_3.id) }
+    let!(:ii_2) { InvoiceItem.create!(quantity: 20, unit_price: 400, status: 2, item_id: item_3.id, invoice_id: invoice_3.id) }
+
+    let!(:invoice_4) { cust_2.invoices.create!(status: 1) }
+    let!(:ii_3) { InvoiceItem.create!(quantity: 4, unit_price: 500, status: 2, item_id: item_4.id, invoice_id: invoice_4.id) }
+
+    let!(:transaction_1) { invoice_3.transactions.create!(credit_card_number: 4536896898764278, credit_card_expiration_date: '12/24', result: 1) }
+    let!(:transaction_2) { invoice_4.transactions.create!(credit_card_number: 4886443388914010, credit_card_expiration_date: '2/34', result: 1) }
+
+    describe '#total_revenue' do
+      it 'returns the total revenue for an invoice' do
+        invoice_1 = create(:invoice_with_items, item_count: 2, ii_qty: 5, ii_price: 3000)
+        invoice_2 = create(:invoice_with_items, item_count: 3, ii_qty: 2, ii_price: 2500)
+
+        expect(invoice_1.total_revenue).to eq(30000)
+        expect(invoice_2.total_revenue).to eq(15000)
+      end
+    end
+
+    describe 'invoice_discount' do 
+      it 'returns the amount to be discounted from an invoice' do 
+        expect(invoice_3.invoice_discount(merchant_4)).to eq(24)
+      end
+    end
+  end 
 end
